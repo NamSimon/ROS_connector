8b54aa74cd3feb5441c8891f154bbba27a611674
NamSimon <cknam0708@sju.ac.kr>
2024-12-01 12:13:14 +0000

    2024.12.01

--- src/start.sh
+++ src/start.sh
@@ -2,4 +2,4 @@
 source /opt/ros/humble/setup.bash   
 source ~/.bashrc && source /root/ROS_connector/install/setup.bash
 cd /root/ROS_connector&& source /root/ROS_connector/install/setup.bash
-ros2 run ros_bridge_manager ros_bridge_manager
+ros2 run ros_connector_manager ros_connector_manager
