1427171b124df59bad4d4c12d22eaaa73e2228cd
NamSimon <cknam0708@sju.ac.kr>
2024-12-01 13:18:24 +0000

    2024.12.01

--- src/ros_connector_manager/setup.py
+++ src/ros_connector_manager/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'ros_connector_manager = ros_connector_manager:main',
+            'ros_connector_manager = ros_connector_manager.ros_connector_manager:main',
         ],
     },
 )
