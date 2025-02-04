6948d87276d22c6341fd2f70cdbca9531af4a34f
NamSimon <cknam0708@sju.ac.kr>
2025-02-04 04:33:35 +0000

    2025.02.04

--- src/ros_connector_manager/ros_connector_manager/ros_connector_manager.py
+++ src/ros_connector_manager/ros_connector_manager/ros_connector_manager.py
@@ -68,41 +68,41 @@ def main():
     rclpy.init()
 
     # # 환경 변수에서 ROS_CONFIG 가져오기
-    # bridges_config_str = os.getenv('ROS_CONFIG')
-    # if not bridges_config_str:
-    #     raise ValueError("ROS_CONFIG 환경 변수가 설정되지 않았습니다.")
+    bridges_config_str = os.getenv('ROS_CONFIG')
+    if not bridges_config_str:
+        raise ValueError("ROS_CONFIG 환경 변수가 설정되지 않았습니다.")
 
-    # # Python 형식 문자열을 파싱
-    # try:
-    #     bridges_config = ast.literal_eval(bridges_config_str)  # JSON 대신 Python 파싱
-    # except (ValueError, SyntaxError) as e:
-    #     print(f"ROS_CONFIG 값: {bridges_config_str}")
-    #     raise ValueError(f"ROS_CONFIG 파싱 에러: {e}")
+    # Python 형식 문자열을 파싱
+    try:
+        bridges_config = ast.literal_eval(bridges_config_str)  # JSON 대신 Python 파싱
+    except (ValueError, SyntaxError) as e:
+        print(f"ROS_CONFIG 값: {bridges_config_str}")
+        raise ValueError(f"ROS_CONFIG 파싱 에러: {e}")
     # bridges_config 예시 파일
     
-    bridges_config=[
+    # bridges_config=[
+    # #         {
+    # #             "type": "short_topic",
+    # #             "ros_topic": "/example/short_topic",
+    # #             "ros_type": "std_msgs/msg/String",
+    # #             "mode": "pub",
+    # #             "platform": "edge"
+    # #         },
+    # #         {
+    # #             "type": "huge_topic",
+    # #             "ros_topic": "/example/huge_topic",
+    # #             "ros_type": "sensor_msgs/msg/Image",
+    # #             "mode": "sub",
+    # #             "platform": "edge"
+    # #         },
     #         {
-    #             "type": "short_topic",
-    #             "ros_topic": "/example/short_topic",
-    #             "ros_type": "std_msgs/msg/String",
-    #             "mode": "pub",
-    #             "platform": "edge"
-    #         },
-    #         {
-    #             "type": "huge_topic",
-    #             "ros_topic": "/example/huge_topic",
-    #             "ros_type": "sensor_msgs/msg/Image",
-    #             "mode": "sub",
-    #             "platform": "edge"
-    #         },
-            {
-                "type": "service",
-                "ros_service": "empty_service",
-                "ros_type": "std_srvs/srv/Empty",
-                "mode": "client",
-                "platform": "edge"
-            }
-        ]
+    #             "type": "service",
+    #             "ros_service": "empty_service",
+    #             "ros_type": "std_srvs/srv/Empty",
+    #             "mode": "client",
+    #             "platform": "user"
+    #         }
+    #     ]
     
     
     # ROSBridgeManager 실행
